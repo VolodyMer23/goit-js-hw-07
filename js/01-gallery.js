e57421ed56ae358bd874411c7d8ae8a979ac7a78
Volodymyr <mv.design8@gmail.com>
2023-03-02 00:15:19 +0200

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector(".gallery");
+
+function galleryMarkUp(galleryItems) {
+  const imageMarkUp = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${preview}">
+                    <img
+                        class="gallery__image"
+                        target="_parent"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>
+      `
+    })
+    .join("");
+  return imageMarkUp;
+}
+
+function addMarkUp() {
+  galleryContainerEl.insertAdjacentHTML(
+    "afterbegin",
+    galleryMarkUp(galleryItems)
+  );
+}
+addMarkUp();
